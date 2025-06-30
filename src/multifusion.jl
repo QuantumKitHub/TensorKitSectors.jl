ccf3067e14c10a45bd712ed2132d13e49a13bcaf
Lukas Devos <ldevos98@gmail.com>
2025-06-30 12:11:41 +0200

    fix small fixes

--- src/multifusion.jl
+++ src/multifusion.jl
@@ -12,9 +12,8 @@ struct IsingBimod <: Sector
     function IsingBimod(row::Int, col::Int, label::Int)
         1 <= row <= 2 && 1 <= col <= 2 ||
             throw(DomainError(lazy"Invalid subcategory ($row, $col)"))
-        0 <= label <= (row == col)
+        0 <= label <= (row == col) ||
             throw(ArgumentError(lazy"Invalid label $label for IsingBimod subcategory ($row, $col)"))
-        end
         return new(row, col, label)
     end
 end
