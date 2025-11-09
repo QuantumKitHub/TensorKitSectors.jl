022c36835e2c89fd0cbbd7c65b81604f4fdb7757
Lukas Devos <ldevos98@gmail.com>
2025-11-09 08:20:48 -0500

    absorb testsetup in testsuite

--- test/testsuite.jl
+++ test/testsuite.jl
@@ -1,18 +1,37 @@
 """
     module SectorTestSuite
 
-Lightweight testsuite registration for sector tests, inspired by the GPUArrays
-testsuite style. Each logical group of tests is registered under a string key
-and can be iterated for every sector type.
+Test suite and utilities that ensure a reusable way of verifying the required interface for a `Sector` type.
+Framework based on the GPUArrays testsuite.
+
+Downstream packages may include this test suite as follows:
+
+```julia
+import TensorKitSectors
+testsuite_path = joinpath(
+    dirname(dirname(pathof(TensorKitSectors))), # TensorKitSectors root
+    "test", "testsuite.jl"
+)
+include(testsuite_path)
+
+SectorTestSuite.test_sectortype(MySectorType)
+```
+
+Additionally, this test suite exports the following convenience testing utilities:
+* [`smallset`](@ref)
+* [`randsector`](@ref)
+* [`hasfusiontensor`](@ref)
 """
 module SectorTestSuite
 
-export tests, @testsuite, @testinferred
+export smallset, randsector, hasfusiontensor
 
 using Test
 using TestExtras
 using TensorKitSectors
-const TKS = TensorKitSectors
+using TensorKitSectors: type_repr
+using Random
+using Base.Iterators: take, product
 
 const tests = Dict()
 
@@ -21,7 +40,8 @@ const tests = Dict()
         # test code here
     end
 
-Register a sector testsuite. The body is executed with a single argument `I`, the concrete `Sector` type under test.
+Register a sector testsuite.
+The body is executed with a single argument `I`, the concrete `Sector` type under test.
 """
 macro testsuite(name, ex)
     safe_name = lowercase(replace(replace(name, " " => "_"), "/" => "_"))
@@ -34,9 +54,11 @@ macro testsuite(name, ex)
 end
 
 """
-Runs the entire TensorKitSectors test suite on sector type `I`
+    test_sectortype(I::Type)
+
+Runs the entire TensorKitSectors test suite on sector type `I`.
 """
-function test(I::Type)
+function test_sectortype(I::Type)
     return @testset "$(TKS.type_repr(I))" begin
         for (name, fun) in tests
             code = quote
@@ -47,6 +69,11 @@ function test(I::Type)
     end
 end
 
+"""
+    @testinferred [AllowedTypes] ex
+
+Like `Test.@inferred`, but registers failures through a test, rather than an error.
+"""
 macro testinferred(ex)
     return _inferred(ex, __module__)
 end
@@ -54,6 +81,7 @@ macro testinferred(ex, allow)
     return _inferred(ex, __module__, allow)
 end
 
+# Implementation copied from Test._inferred:
 function _inferred(ex, mod, allow = :(Union{}))
     if Meta.isexpr(ex, :ref)
         ex = Expr(:call, :getindex, ex.args...)
@@ -99,7 +127,41 @@ function _inferred(ex, mod, allow = :(Union{}))
     return Base.remove_linenums!(result)
 end
 
-include("testsetup.jl")
+smallset(::Type{I}) where {I <: Sector} = take(values(I), 5)
+function smallset(::Type{ProductSector{Tuple{I1, I2}}}) where {I1, I2}
+    iter = product(smallset(I1), smallset(I2))
+    s = collect(i ⊠ j for (i, j) in iter if dim(i) * dim(j) <= 6)
+    return length(s) > 6 ? rand(s, 6) : s
+end
+function smallset(::Type{ProductSector{Tuple{I1, I2, I3}}}) where {I1, I2, I3}
+    iter = product(smallset(I1), smallset(I2), smallset(I3))
+    s = collect(i ⊠ j ⊠ k for (i, j, k) in iter if dim(i) * dim(j) * dim(k) <= 6)
+    return length(s) > 6 ? rand(s, 6) : s
+end
+
+function randsector(::Type{I}) where {I <: Sector}
+    s = collect(smallset(I))
+    a = Random.rand(s)
+    while isunit(a) # don't use trivial label
+        a = Random.rand(s)
+    end
+    return a
+end
+randsector(::Type{I}) where {I <: Union{Trivial, PlanarTrivial}} = unit(I)
+
+function hasfusiontensor(I::Type{<:Sector})
+    try
+        fusiontensor(unit(I), unit(I), unit(I))
+        return true
+    catch e
+        if e isa MethodError
+            return false
+        else
+            rethrow(e)
+        end
+    end
+end
+
 include("sectors.jl")
 
 end # module SectorTestSuite
