a9ae96a80a4ee1565eeeadb737aa13d3d1f5f132
leburgel <lander.burgelman@gmail.com>
2025-11-20 20:04:31 +0100

    Set up documentation

--- src/product.jl
+++ src/product.jl
@@ -8,7 +8,7 @@ const SectorTuple = Tuple{Vararg{Sector}}
 
 Represents the Deligne tensor product of sectors. The type parameter `T` is a tuple of the
 component sectors. The recommended way to construct a `ProductSector` is using the
-[`deligneproduct`](@ref) (`⊠`) operator on the components.
+[`deligneproduct`](@ref ⊠) (`⊠`) operator on the components.
 """
 struct ProductSector{T <: SectorTuple} <: Sector
     sectors::T
