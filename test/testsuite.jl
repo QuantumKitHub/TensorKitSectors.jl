6f8ab3ea1aea60c4f9d2b13818fedae2e7a3ed57
Lukas Devos <ldevos98@gmail.com>
2025-11-30 17:38:28 -0500

    Add a reusable test suite (#40)

---
+++ test/testsuite.jl
@@ -0,0 +1,109 @@
+"""
+    module SectorTestSuite
+
+Test suite and utilities that ensure a reusable way of verifying the required interface for a `Sector` type.
+Framework based on the GPUArrays testsuite.
+
+Downstream packages may include this test suite as follows:
+
+```julia
+import TensorKitSectors
+testsuite_path = joinpath(
+    dirname(dirname(pathof(TensorKitSectors))), # TensorKitSectors root
+    "test", "testsuite.jl"
+)
+include(testsuite_path)
+
+SectorTestSuite.test_sector(MySectorType)
+```
+
+Additionally, this test suite exports the following convenience testing utilities:
+* [`smallset`](@ref)
+* [`randsector`](@ref)
+* [`hasfusiontensor`](@ref)
+"""
+module SectorTestSuite
+
+export smallset, randsector, hasfusiontensor
+
+using Test
+using TestExtras
+using TensorKitSectors
+using TensorKitSectors: type_repr
+using Random
+using Base.Iterators: take, product
+
+const tests = Dict()
+
+"""
+    @testsuite name I -> begin
+        # test code here
+    end
+
+Register a sector testsuite.
+The body is executed with a single argument `I`, the concrete `Sector` type under test.
+"""
+macro testsuite(name, ex)
+    safe_name = lowercase(replace(replace(name, " " => "_"), "/" => "_"))
+    fn = Symbol("test_$(safe_name)")
+    return quote
+        $(esc(fn))(I) = $(esc(ex))(I)
+        @assert !haskey(tests, $name) "duplicate testsuite name: $name"
+        tests[$name] = $fn
+    end
+end
+
+"""
+    test_sectortype(I::Type)
+
+Runs the entire TensorKitSectors test suite on sector type `I`.
+"""
+function test_sector(I::Type)
+    return @testset "$(type_repr(I))" begin
+        for (name, fun) in tests
+            code = quote
+                $fun($I)
+            end
+            @eval @testset $name $code
+        end
+    end
+end
+
+smallset(::Type{I}) where {I <: Sector} = take(values(I), 5)
+function smallset(::Type{ProductSector{Tuple{I1, I2}}}) where {I1, I2}
+    iter = product(smallset(I1), smallset(I2))
+    s = collect(i ⊠ j for (i, j) in iter if dim(i) * dim(j) <= 6)
+    return length(s) > 6 ? rand(s, 6) : s
+end
+function smallset(::Type{ProductSector{Tuple{I1, I2, I3}}}) where {I1, I2, I3}
+    iter = product(smallset(I1), smallset(I2), smallset(I3))
+    s = collect(i ⊠ j ⊠ k for (i, j, k) in iter if dim(i) * dim(j) * dim(k) <= 6)
+    return length(s) > 6 ? rand(s, 6) : s
+end
+
+function randsector(::Type{I}) where {I <: Sector}
+    s = collect(smallset(I))
+    a = Random.rand(s)
+    while isunit(a) # don't use trivial label
+        a = Random.rand(s)
+    end
+    return a
+end
+randsector(::Type{I}) where {I <: Union{Trivial, PlanarTrivial}} = unit(I)
+
+function hasfusiontensor(I::Type{<:Sector})
+    try
+        fusiontensor(unit(I), unit(I), unit(I))
+        return true
+    catch e
+        if e isa MethodError
+            return false
+        else
+            rethrow(e)
+        end
+    end
+end
+
+include("sectors.jl")
+
+end # module SectorTestSuite
