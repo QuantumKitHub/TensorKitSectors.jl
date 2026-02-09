7a14dab7280224097756ea92f548ff242d08daef
Boris De Vos <143942306+borisdevos@users.noreply.github.com>
2026-02-09 19:03:35 +0100

    Convenient additions to `TimeReversed` (#63)
    * convenient additions to `TimeReversed`
    
    * attempt at better show
    
    * Revert "attempt at better show"
    
    This reverts commit a6af69390b955303cedddaa30ffa72a0d3aebef4.
    
    * fix pretty printing
    
    * lowercase function
    
    * potential fix for symbols being annoying
    
    * Revert "potential fix for symbols being annoying"
    
    This reverts commit 86e17565c83e1967dbb20553aac019e6414c10dd.
    
    * printing and conversion magic!
    
    * add docstring and fix existing
    
    * suggestions
    
    * remove fusiontensor + make TR identity for symmetric braiding
    
    * also specialize Asymbol and Bsymbol
    
    * small formatting changes
    
    ---------
    
    Co-authored-by: Lukas Devos <ldevos98@gmail.com>

--- src/timereversed.jl
+++ src/timereversed.jl
@@ -2,40 +2,48 @@
 
 """
     struct TimeReversed{I <: Sector}
-    TimeReversed(a::I) where {I <: Sector}
+    TimeReversed(a::Sector)
+    timereversed(a::Sector)
 
-Represents the time-reversed version of the sector `I`, i.e. the
+Represents the time-reversed version of the sector `a`, i.e. the
 sector with the same fusion rules and `F`-symbols, but with the
 inverse braiding.
+Time reversal acts trivially on sectors with symmetric braiding.
+In such cases, `timereversed(a)` simply returns `a`.
 """
 struct TimeReversed{I <: Sector} <: Sector
     a::I
-    function TimeReversed{I}(a::I) where {I <: Sector}
-        if BraidingStyle(I) isa NoBraiding
+    function TimeReversed{I}(a) where {I <: Sector}
+        BraidingStyle(I) isa NoBraiding &&
             throw(ArgumentError("TimeReversed is not defined for sectors $I with no braiding"))
-        end
         return new{I}(a)
     end
 end
+
+TimeReversed(a::Sector) = TimeReversed{typeof(a)}(a)
+
+"""
+    timereversed(a::Sector)
+
+Return the time-reversed version of the sector `a`. 
+If `a` is already a time-reversed sector, return the original sector.
+"""
+timereversed(a::Sector) = BraidingStyle(typeof(a)) isa SymmetricBraiding ? a : TimeReversed(a)
+timereversed(a::TimeReversed) = a.a
+
 FusionStyle(::Type{TimeReversed{I}}) where {I <: Sector} = FusionStyle(I)
 BraidingStyle(::Type{TimeReversed{I}}) where {I <: Sector} = BraidingStyle(I)
-function Nsymbol(
-        a::TimeReversed{I}, b::TimeReversed{I}, c::TimeReversed{I}
-    ) where {I <: Sector}
-    return Nsymbol(a.a, b.a, c.a)
-end
 
-function Fsymbol(
-        a::TimeReversed{I}, b::TimeReversed{I}, c::TimeReversed{I},
-        d::TimeReversed{I}, e::TimeReversed{I}, f::TimeReversed{I}
-    ) where {I <: Sector}
-    return Fsymbol(a.a, b.a, c.a, d.a, e.a, f.a)
-end
-function Rsymbol(
-        a::TimeReversed{I}, b::TimeReversed{I}, c::TimeReversed{I}
-    ) where {I <: Sector}
-    return adjoint(Rsymbol(a.a, b.a, c.a))
-end
+Nsymbol(a::I, b::I, c::I) where {I <: TimeReversed} = Nsymbol(a.a, b.a, c.a)
+
+Fsymbol(a::I, b::I, c::I, d::I, e::I, f::I) where {I <: TimeReversed} =
+    Fsymbol(a.a, b.a, c.a, d.a, e.a, f.a)
+Asymbol(a::I, b::I, c::I) where {I <: TimeReversed} = Asymbol(a.a, b.a, c.a)
+Bsymbol(a::I, b::I, c::I) where {I <: TimeReversed} = Bsymbol(a.a, b.a, c.a)
+
+dim(c::TimeReversed) = dim(c.a)
+
+Rsymbol(a::I, b::I, c::I) where {I <: TimeReversed} = adjoint(Rsymbol(a.a, b.a, c.a))
 
 unit(::Type{TimeReversed{I}}) where {I <: Sector} = TimeReversed{I}(unit(I))
 allunits(::Type{TimeReversed{I}}) where {I <: Sector} = SectorSet{TimeReversed{I}}(TimeReversed{I}, allunits(I))
@@ -75,3 +83,12 @@ fusionscalartype(::Type{TimeReversed{I}}) where {I} = fusionscalartype(I)
 braidingscalartype(::Type{TimeReversed{I}}) where {I} = braidingscalartype(I)
 sectorscalartype(::Type{TimeReversed{I}}) where {I} = sectorscalartype(I)
 dimscalartype(::Type{TimeReversed{I}}) where {I} = dimscalartype(I)
+
+type_repr(::Type{TimeReversed{I}}) where {I <: Sector} = "TimeReversed{$(type_repr(I))}"
+function Base.show(io::IO, c::TimeReversed{I}) where {I <: Sector}
+    print_info = get(io, :typeinfo, nothing) !== typeof(c)
+    print_info && print(io, type_repr(typeof(c)), "(")
+    show(IOContext(io, :typeinfo => I), c.a)
+    print_info && print(io, ")")
+    return nothing
+end
