f8e5ea5a5bf52f4bd3a4f84bab5cef6540ae74e0
Chenqi Meng <mcqgravity@gmail.com>
2026-04-14 20:07:58 +0800

    slightly simplify by using _sectors

--- src/product.jl
+++ src/product.jl
@@ -26,8 +26,7 @@ _sectors(::Type{ProductSector{T}}) where {T} = Base.fieldtypes(T)
 _sectors(::Type) = error("should never be reached") # keeps JET happy
 
 function _kron_iter(::Type{ProductSector{T}}) where {T} # Construct an iterator with the same order as the Kronecker product.
-    sector_tuple = Base.fieldtypes(T)
-    tuple_iterators = values.(sector_tuple)
+    tuple_iterators = values.(_sectors(T))
     @assert !any(it -> Base.IteratorSize(it) isa Base.IsInfinite, tuple_iterators) "All sectors need to be finite"
     return (ProductSector{T}(reverse(x)) for x in Iterators.product(reverse(tuple_iterators)...))
 end
